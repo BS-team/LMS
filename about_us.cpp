f9a22977bdd7242de2ab290f1d035f53f52f82f7
TRIPLEM <mazaherimohamad4@gmail.com>
2020-08-27 12:47:07 +0430

    MY FIRST COMMIT AFTER CHANGES

--- about_us.cpp
+++ about_us.cpp
@@ -1,18 +1,19 @@
 #include "about_us.h"
 #include "ui_about_us.h"
 #include"mainwindow.h"
+
 About_us::About_us(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::About_us)
 {
     ui->setupUi(this);
 
-    QPixmap pix("D:/FinalProjectTerm2/erfanN.jpg");
+    QPixmap pix("E:/LastOne/ProjectOfTerm2/erfanN.jpg");
     ui->label->setPixmap(pix);
-    QPixmap pix1("D:/FinalProjectTerm2/aliP.jpg");
+    QPixmap pix1("E:/LastOne/ProjectOfTerm2/aliP.jpg");
     ui->label_3->setPixmap(pix1);
 
-    QPixmap pix2("D:/FinalProjectTerm2/mohamadM.jpg");
+    QPixmap pix2("E:/LastOne/ProjectOfTerm2/mohamadM.jpg");
     ui->label_4->setPixmap(pix2);
 
 
@@ -21,7 +22,30 @@ About_us::About_us(QWidget *parent) :
     pal.setColor(QPalette::Background, Qt::blue);
     this->setAutoFillBackground(true);
     this->setPalette(pal);
-    this->setWindowTitle("Main Page");
+    this->setWindowTitle("درباره ما");
+
+    //ui->back->setStyleSheet("background-color: yellow");
+    ui->back->setStyleSheet(QString::fromUtf8("QPushButton{"
+    "padding: -6px;"
+    //   "background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
+       //                                                                                "stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,"
+        //                                                                                "stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);"
+    //"selection-background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 178, 102, 255), stop:0.55 rgba(235, 148, 61, 255), stop:0.977528 rgba(60, 60, 60, 255), stop:1 rgba(0, 0, 0, 0));"
+   // "background-color: lightgray;"
+                                                "  left: -2px;"
+    "min-width: 8em;"
+    "border-style: solid;"
+    "border-color: black;"
+    "border-width: 3px;"
+    //"subcontrol-position: center;"
+    //"border-left-style: outset;"
+      //"border-right-style: outset;"
+               "selection-color: yellow;"
+                 "selection-background-color: blue;"
+
+    "border-bottom-left-radius: 30px;"
+    "border-top-right-radius: 30px;"
+    "}"));
 }
 
 About_us::~About_us()
